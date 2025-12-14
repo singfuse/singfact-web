1abfdf27d190596b0155507cfdf597cc90aca22a
Vinoth Kannan <svkn.87@gmail.com>
2025-12-14 20:13:01 +0530

    Initial commit
    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <a href="https://apps.apple.com/sg/app/singfact/id6602890953?itscg=30200&itsct=apps_box_badge&mttnsubad=6602890953" style={{ display: "inline-block" }} title="Download on the App Store">
+      <img src="https://toolbox.marketingtools.apple.com/api/v2/badges/download-on-the-app-store/white/en-us?releaseDate=1735948800" alt="Download on the App Store" style={{ verticalAlign: "middle", objectFit: "contain" }} className="h-12 lg:h-14" />
+    </a>
+  );
+}
